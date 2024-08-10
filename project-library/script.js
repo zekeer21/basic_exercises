54a4ed5101d93362dc38b77b21e2edff9db4631c
zekeer21 <achas241@gmail.com>
2024-08-10 10:59:18 +0800

    "Minor updates"

--- project-library/script.js
+++ project-library/script.js
@@ -1,12 +1,46 @@
+const myBooks = [];
+
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const addBook = document.querySelector("#btn-add");
-const cancelAdd = document.querySelector(".btn-cacel");
+const cancelAdd = document.querySelector(".btn-cancel");
+const submit = document.querySelector("#submit");
 
-const addBookModal = function(){
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const isRead = document.querySelector("#isRead");
+
+
+const openModal = function(){
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
     addBook.classList.add("hidden");
 }
 
-addBook.addEventListener("click", addBookModal);
+const closeModal = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    addBook.classList.remove("hidden");
+}
+
+const getData = function(){
+    const titleValue = title.value;
+    const authorValue = author.value;
+    const pagesValue = pages.value;
+    const isReadValue = isRead.value;
+    console.log(titleValue, authorValue,pagesValue,isReadValue);
+}
+
+addBook.addEventListener("click", openModal);
+cancelAdd.addEventListener("click", closeModal);
+console.log(submit.addEventListener("click", getData));
+// console.log(getData());
+
+// function Book(title, author, pages, isRead){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.isRead = isRead;
+// }
+
